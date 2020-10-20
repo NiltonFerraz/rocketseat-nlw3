b5da89818c22d92f90aa63bd82448eb8561c4c72
Nilton Ferraz <nilferraz@gmail.com>
2020-10-20 07:42:08 -0300

    first commit

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,30 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+    
+}
+
+
+//create map
+const map = L.map("mapid", options).setView([-27.2206249,-49.648594], 15);
+
+//create and add tilelayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" ).addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg", 
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+L.marker([-27.2206249,-49.648594], { icon })
+  .addTo(map)
+
+  
